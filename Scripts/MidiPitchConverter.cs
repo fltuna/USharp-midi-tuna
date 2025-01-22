6459778b799d56198c6655d715119fb6dfcb1a50
faketuna <faketuna.st@gmail.com>
2025-01-23 02:12:26 +0900

    fix: forget to git add MinPitch edit

--- Scripts/MidiPitchConverter.cs
+++ Scripts/MidiPitchConverter.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 
 public static class MidiPitchConverter
 {
-    private const float MinPitch = 0f;
+    private const float MinPitch = 0.03125f;
     private const float MaxPitch = 15.10199f;
     private const int BaseC = 60; // C5
     private const int NotesPerOctave = 12;
